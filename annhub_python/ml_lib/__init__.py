e26867e0f9841939508d0a16fad3156d692dae18
Bao Phuc Nguyen <phuc.nguyenbao@hcmut.edu.vn>
2021-10-05 14:17:34 +0700

    update path

--- annhub_python/ml_lib/__init__.py
+++ annhub_python/ml_lib/__init__.py
@@ -1,1 +1,14 @@
+import requests
+def download():
+    URL = "https://github.com/ans-ari/annhub-python/raw/update-library/annhub_python/ml_lib/annhub.pyd"
+    
+    response = requests.get(url=URL,allow_redirects=True)
+    file_name = "annhub.pyd"
+    with open(file_name,'wb') as f:
+        f.write(response.content)
+download()
+
 from . import annhub as annhub
+
+
+
